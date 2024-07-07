df3baf8ea719409c22d1164d1fd4983a9865dfa9
ibrahimkocal <ibrahiimkocal@gmail.com>
2024-07-07 16:30:03 +0300

    showTime fonksiyonu ile saat bilgisinin sürekli güncellenmesi sağlandı

--- js/clock.js
+++ js/clock.js
@@ -4,17 +4,21 @@ let myClock = document.querySelector("#myClock");
 let myNameInput = prompt("Adınızı giriniz: ");
 myName.innerHTML = myNameInput;
 
+function showTime(){
+    const date = new Date();
 
-const date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
 
-let hour = date.getHours();
-let minute = date.getMinutes();
-let second = date.getSeconds();
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second: second;
 
-hour = hour < 10 ? "0" + hour : hour;
-minute = minute < 10 ? "0" + minute : minute;
-second = second < 10 ? "0" + second: second;
+    myClock.innerHTML = `${hour} : ${minute} : ${second} `;
 
-myClock.innerHTML = `${hour} : ${minute} : ${second} `
+    setTimeout(showTime, 1000);
+}
 
+showTime();
 
