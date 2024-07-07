11daa961609ba01d3a1b3dafced2e4165ce8a2e3
ibrahimkocal <ibrahiimkocal@gmail.com>
2024-07-07 16:20:02 +0300

    saat, dakika ve saniye'nin 10'dan küçük olma durumlarında başlarına 0 eklenecek şekilde düzenleme yapıldı.

--- js/clock.js
+++ js/clock.js
@@ -4,11 +4,16 @@ let myClock = document.querySelector("#myClock");
 let myNameInput = prompt("Adınızı giriniz: ");
 myName.innerHTML = myNameInput;
 
+
 const date = new Date();
 
 let hour = date.getHours();
 let minute = date.getMinutes();
 let second = date.getSeconds();
+
+hour = hour < 10 ? "0" + hour : hour;
+minute = minute < 10 ? "0" + minute : minute;
+second = second < 10 ? "0" + second: second;
 
 myClock.innerHTML = `${hour} : ${minute} : ${second} `
 
